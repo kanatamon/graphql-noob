7f0c44143980fcd90a0b6e3638c81e1cea8195b5
DESKTOP-15GUKDC\Kantapon Sritiyot <kantpaon.sritiyot@gmail.com>
2018-06-19 15:59:01 +0700

    Remove `app.get`

--- storage/lib/index.js
+++ storage/lib/index.js
@@ -1,28 +1,16 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import { graphql } from 'graphql';
 import rootSchema from './rootSchema';
 
 const PORT = 3000;
 
 const app = express();
 
-app.use('/graphiql', graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
   schema: rootSchema,
   pretty: true,
   graphiql: true,
 }));
-
-app.get('/graphql', (req, res) => {
-  const graphqlQuery = req.query.graphqlQuery;
-  if (!graphqlQuery) {
-    return res.status(500).send('You must provide a query');
-  }
-  return graphql(rootSchema, graphqlQuery)
-    .then(response => response.data)
-    .then((data) => res.json(data))
-    .catch((err) => console.error(err));
-});
 
 app.listen(PORT, () => {
   console.log('Server running on port ', PORT);
