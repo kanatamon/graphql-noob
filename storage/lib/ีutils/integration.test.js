309a6c3856f95cc0013a79d3066789d42a4ebb72
DESKTOP-15GUKDC\Kantapon Sritiyot <kantpaon.sritiyot@gmail.com>
2018-06-20 10:34:29 +0700

    Add mock for `getJokes` resolved fragile test due to unstable network

--- storage/lib/ีutils/integration.test.js
+++ storage/lib/ีutils/integration.test.js
@@ -1,13 +1,21 @@
 import * as integrationServer from './integrationServer';
+import * as jokeApiMock from '../joke/api';
 
 describe('intregation', () => {
   let app;
+  const spy = jest.spyOn(jokeApiMock, 'getJokes');
+
   beforeEach(done => {
     app = integrationServer.start(9000, done);
   });
 
   afterEach(done => {
     integrationServer.stop(app, done);
+    spy.mockReset();
+  });
+
+  afterAll(() => {
+    spy.mockRestore();
   });
 
   test('should resolve a list of jokes', async () => {
@@ -17,9 +25,26 @@ describe('intregation', () => {
         joke
       }
     }`;
-    const result = await integrationServer.graphqlQuery(app, query);
-    expect(result).toHaveProperty('data');
-    expect(result.data).toHaveProperty('jokes');
-    expect(result.data.jokes).toEqual(expect.any(Array));
+    const expected = {
+      data: {
+        jokes: [
+          {
+            id: "1",
+            joke: 'Chuck Norris uses ribbed condoms inside out, so he gets the pleasure.'
+          },
+          {
+            id: "2",
+            joke: 'MacGyver can build an airplane out of gum and paper clips. Chuck Norris can kill him and take it.'
+          },
+        ]
+      },
+    };
+    jokeApiMock.getJokes.mockResolvedValue({
+      data: expected.data.jokes,
+      error: null,
+    });
+    await expect(integrationServer.graphqlQuery(app, query))
+      .resolves
+      .toEqual(expected);
   });
 });
